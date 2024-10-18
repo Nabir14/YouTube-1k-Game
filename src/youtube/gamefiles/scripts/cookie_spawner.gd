2164bebe6b9834dfcc58a33d4f175beda3e3ceb1
Nabir14 <mushfiquefarhannabir@gmail.com>
2024-10-18 19:30:43 +0600

    v1.5 lol

--- src/youtube/gamefiles/scripts/cookie_spawner.gd
+++ src/youtube/gamefiles/scripts/cookie_spawner.gd
@@ -3,18 +3,16 @@ extends Node3D
 @onready var timer : Timer = $Timer
 var cookie : PackedScene = preload("res://gamefiles/scenes/cookie.tscn")
 @export var scatterAmount : int = 16
-var i = 0
 
 func _ready() -> void:
-	while i < scatterAmount:
-		var x = randi_range(-128, 128)
-		var z = randi_range(-128, 128)
-		var ci = cookie.instantiate()
-		ci.position = Vector3(x, 1, z)
-		add_child(ci)
-		i += 1
+	spawn()
 
 func _on_timer_timeout() -> void:
+	spawn()
+	timer.start()
+
+func spawn():
+	var i : int = 0
 	while i < scatterAmount:
 		var x = randi_range(-128, 128)
 		var z = randi_range(-128, 128)
@@ -22,4 +20,3 @@ func _on_timer_timeout() -> void:
 		ci.position = Vector3(x, 1, z)
 		add_child(ci)
 		i += 1
-	timer.start()
